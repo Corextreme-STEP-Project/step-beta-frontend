dabe692984a87b3064cf6d3bd76fe47dd0b4a0f8
Rob-bie-dot <naaayettey12@gmail.com>
2024-11-01 14:05:27 +0000

    Created pages folder,  added files for loginform and registerform. Created services folder, added auth.js file. Created the  .env file for the base url. Started working in the loginform.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout/layout";
+import LoginForm from "./assets/pages/logins/LoginForm";
+import RegistrationForm from "./assets/pages/logins/RegistrationForm";
 
 function App() {
   const router = createBrowserRouter ([
 {
   path:"/",
   element: <Layout/>
-}
+},
+{
+  path:"/login",
+  element: <LoginForm/>,
+},
+{
+  path:"/register",
+  element: <RegistrationForm/> ,
+},
 
   ])
 
